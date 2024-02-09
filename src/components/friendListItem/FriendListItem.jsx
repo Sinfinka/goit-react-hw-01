962341af1b7c5461f1c1bf89a0974b2142832cf2
Sinfinka <inadaradar@gmail.com>
2024-02-09 23:47:03 +0200

    add css for friend list

--- src/components/friendListItem/FriendListItem.jsx
+++ src/components/friendListItem/FriendListItem.jsx
@@ -1,17 +1,28 @@
+import React from "react";
 import friendData from "./../../input-data/friendData.json";
+import css from "./friendListItem.module.css";
+import clsx from "clsx";
 
 console.log(friendData);
+console.log(css);
 
 export const FriendListIItem = (props) => {
   return (
-    <ul className="ddd">
-      {props.friendData.map((friend) => (
-        <li className="efrf" key={friend.id}>
-          <img src={friend.avatar} alt="Friend avatar" />
-          <p>{friend.name} </p>
-          <p>{friend.isOnline} </p>
-        </li>
-      ))}
+    <ul className={css.friendList}>
+      {props.friendData.map((friend) => {
+        const isOnline = clsx(css.status, friend.isOnline && css.isOnline);
+        return (
+          <li className={css.friend} key={friend.id}>
+            <img
+              className={css.avatar}
+              src={friend.avatar}
+              alt="Friend avatar"
+            />
+            <p className={css.name}>{friend.name} </p>
+            <p className={isOnline}>{friend.isOnline ? "Online" : "Offline"}</p>
+          </li>
+        );
+      })}
     </ul>
   );
 };
