8c50000379f9c7cc2f8754e22e9f60aca3ab2965
Sinfinka <inadaradar@gmail.com>
2024-02-10 18:17:52 +0200

    fixed

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,25 +1,20 @@
-import userData from "../../input-data/userData.json";
-import css from "./Profie.module.css";
-
-console.log(userData);
+import css from "./Profile.module.css";
 
 export const Profile = ({
   userData: { username, tag, location, avatar, stats },
 }) => {
   return (
     <>
-      {/* {username} {tag} {location} {avatar} {stats.followers} {stats.views}{" "}
-      {stats.likes} */}
       <div className={css.conteiner}>
         <div className={css.userInfo}>
           <img className={css.avatar} src={avatar} alt="User avatar" />
-          <p className={css.name}>{username}</p>{" "}
-          <p className={css.tag}>@{tag}</p>{" "}
+          <p className={css.name}>{username}</p>
+          <p className={css.tag}>@{tag}</p>
           <p className={css.location}>{location}</p>
         </div>
         <ul className={css.stats}>
           <li className={css.followers}>
-            <span>Followers</span>{" "}
+            <span>Followers</span>
             <span className={css.value}>{stats.followers}</span>
           </li>
           <li className={css.views}>
